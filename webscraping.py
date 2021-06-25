f0cae2490b1136e4bff4887a91602e3c7afa104d
Zufaina Khalid <31650311+zuffiiee@users.noreply.github.com>
2021-06-25 07:50:21 +0530

    Update webscraping.py

--- webscraping.py
+++ webscraping.py
@@ -6,19 +6,17 @@ print("Googling..")
 
 search_result= requests.get('https://www.google.com/search?q='+key)
 
-# search_result= requests.get('https://www.google.com/search?q='+''.join(sys.argv[1:]))
-# search_result.raise_for_status()
 
 soup= BeautifulSoup(search_result.text,'html.parser')
-# soup= BeautifulSoup(search_result.text,'lxml')
+
 
 # aa=soup.select("div.yuRUbf a")
 # for a in aa:
 # 	print(a.get('href'))
 
-# results=soup.select('a h3')
-# for x in results[:10]:
-# 	print(x.getText())
+results=soup.select('a h3')
+for x in results[:10]:
+	print(x.getText())
 
 
 
@@ -27,16 +25,10 @@ soup= BeautifulSoup(search_result.text,'html.parser')
 # for link in search_links:
 # 	print(link['href'])
 
-all_links=soup.find_all("div", class_="yuRUbf")
-# print(type(all_links))
-print(all_links[0])
+# all_links=soup.find_all("div", class_="yuRUbf")
+# # print(type(all_links))
+# print(all_links[0])
 	
-
-
-
-# all_links=soup.select('title')
-# print(all_links[0].getText())
-
 
 # for tag in soup.find_all('a',{'class':"yuRUbf"}):
 # 	print(tag.get('href'))
